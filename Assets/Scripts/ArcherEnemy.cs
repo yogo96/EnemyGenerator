cf1b33a1d0d2ea229bd85ab715cbf4a0df6952d6
yogo96 <yogomna96@mail.ru>
2024-10-30 19:41:51 +0300

    update

---
+++ Assets/Scripts/ArcherEnemy.cs
@@ -0,0 +1,3 @@
+public class ArcherEnemy : Enemy
+{
+}
--- Assets/Scripts/SpawnerPoint.cs
+++ Assets/Scripts/SpawnerPoint.cs
@@ -3,23 +3,13 @@ using UnityEngine;
 public class SpawnerPoint : MonoBehaviour
 {
     [SerializeField] private Enemy _enemyPrefab;
+    [SerializeField] private TargetPoint _targetPoint;
     
     public void CreateEnemy()
     {
-        Vector3 direction = GetRandomDirection();
+        Vector3 direction = _targetPoint.transform.position;
         Enemy enemy = Instantiate(_enemyPrefab, transform.position, Quaternion.Euler(direction));
         enemy.SetDirection(direction);
     }
 
-    private Vector3 GetRandomDirection()
-    {
-        int minRotationValue = -180;
-        int maxRotationValue = 180;
-
-        int rotationX = Random.Range(minRotationValue, maxRotationValue);
-        int rotationZ = Random.Range(minRotationValue, maxRotationValue);
-        int rotationY = 0;
-
-        return new Vector3(rotationX, rotationY, rotationZ);
-    }
 }
